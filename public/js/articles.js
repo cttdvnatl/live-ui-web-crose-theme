b28dece03dad482f934f16e36cf309d8b05a1115
An Tran <an.tran@cttdvnatl.org>
2019-11-19 21:11:33 -0500

    fixed articles and single-article page

--- public/js/articles.js
+++ public/js/articles.js
@@ -21,7 +21,7 @@ let getArticleString = (title, date, body) => (
                     <!-- <a href="#">Read More <i class="fa fa-angle-double-right"></i></a> -->
                 </div>
                 <div class="find-out-more-btn">
-                    <a href="#" class="btn crose-btn btn-2">Find Out More</a>
+                    <a href="single-article.html?date=${date}" class="btn crose-btn btn-2">Find Out More</a>
                 </div>
             </div>
         </div>
