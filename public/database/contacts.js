9ed8a5d9f5f16ba046c8b80a70bbc9ae2a084f84
An Tran <an.tran@cttdvnatl.org>
2019-11-23 10:53:31 -0500

    add blog-content.js and contacts.js

---
+++ public/database/contacts.js
@@ -0,0 +1,30 @@
+const contacts =
+[
+  {
+    "title": "Pastor",
+    "name": "Cha Tuan",
+    "phone": "4043335555"
+  },
+  {
+    "title": "Parochial Vicar",
+    "name": "Cha Tuan-Anh",
+    "phone": "4043335555"
+  },
+  {
+    "title": "Decon",
+    "name": "Thay Hoa",
+    "phone": "4043335555"
+  },
+  {
+    "title": "Admin. Parish",
+    "name": "Sur. Thuy Nguyen",
+    "phone": "4043335555"
+  },
+  {
+    "title": "Admin. Youth Ministry",
+    "name": "Sur. ",
+    "phone": "4043335555"
+  }
+]
+
+export default contacts
