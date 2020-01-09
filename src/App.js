c37c1002a24b9c0df523d467cacf12ddf8ed7ff2
VanPham <van.k.pham@cttdvnatl.org>
2020-01-08 21:30:07 -0500

    no message

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ import {
 import BlogPage from './pages/BlogPage';
 import BlogListPage from './pages/BlogListPage';
 import Index from './pages/Index';
+import Directors from './pages/Directors';
 function App() {
   return (
     <Router>
     	<div className="App">
 			    <Route path="/blog-list/:id" component={BlogPage} exact />
         	<Route path="/blog-list" component={BlogListPage} exact />
+          <Route path="/directors" component={Directors} exact/>
           <Route path="/" component={Index} exact/>
+          
       	</div>
     </Router>
   ) 
---
+++ src/pages/Directors.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Preloader from '../components/Preloader';
+import Header from '../components/Header';
+
+
+const Directors = (prop) => {
+    return(
+        <div>
+            <Preloader/>
+            <Header/>
+
+        </div>
+    )
+}
+export default Directors;
