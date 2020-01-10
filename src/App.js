5a6f7b10faa495be3b975a35b213436d1965287c
James Zahrobsky <jim.zahrobsky.jr@gmail.com>
2020-01-10 01:14:18 -0500

    Revert "refactor header and carausel to react components"
    This reverts commit a31c69e6cb6372ea61c1273c3b23297f17b3ba70.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
 import React from 'react';
-// import './App.css';
+import './App.css';
+
 import {
   BrowserRouter as Router,
   Route,
 } from 'react-router-dom';
 import BlogPage from './pages/BlogPage';
 import BlogListPage from './pages/BlogListPage';
-import Index from './pages/Index';
+
 function App() {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+  //return '';
   return (
     <Router>
     	<div className="App">
 			    <Route path="/blog-list/:id" component={BlogPage} exact />
         	<Route path="/blog-list" component={BlogListPage} exact />
-          <Route path="/" component={Index} exact/>
       	</div>
     </Router>
   ) 
