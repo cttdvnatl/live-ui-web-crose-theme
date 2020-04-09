9f46424f6ed36696f7cb8eed57e4d41bd44ed1cd
VanPham <van.k.pham@cttdvnatl.org>
2020-04-09 09:28:06 -0400

    provide teacher test data for viet Ngu webpage

--- src/database/vietHongTeachers.js
+++ src/database/vietHongTeachers.js
@@ -1,34 +1,50 @@
 const VietHongTeacherContent = [
     {   
         classification: 'main-teacher',
-        image: '../img/core-img/ChaTuanAnh.jpg',
-        name: 'Teacher Name',
+        image: 'http://www.cttdvnatl.net/gallery/img/core-img/PaulNguyen.jpg',
+        name: 'Phaolô Nguyễn Chinh Paul',
+        email: 'paul.chinh.nguyen@cttdvnatl.org',
+        class: '1A',
+        comment: [``]
+    },
+    {   
+        classification: 'main-teacher',
+        image: 'http://www.cttdvnatl.net/gallery/img/core-img/TrangVu.jpg',
+        name: 'Anna Vũ Thùy Trang',
         email: 'email@gmail.com',
         class: '1A',
         comment: [``]
     },
     {   
         classification: 'main-teacher',
-        image: 'img/core-img/ChaKimPhong.jpg',
-        name: 'Teacher Name',
-        email: 'email@gmail.com',
+        image: 'http://www.cttdvnatl.net/gallery/img/core-img/ThaoLe.jpg',
+        name: 'Theresa Nguyễn Lệ Thảo',
+        email: 'lethao.theresa@cttdvnatl.org',
+        class: '2A',
+        comment: [``]
+    },    
+    {   
+        classification: 'main-teacher',
+        image: 'http://www.cttdvnatl.net/gallery/img/core-img/PaulNguyen.jpg',
+        name: 'Phaolô Nguyễn Chinh Paul',
+        email: 'paul.chinh.nguyen@cttdvnatl.org',
         class: '1A',
+        comment: [``]
+    },    
+    {   
+        classification: 'assistant-teacher',
+        image: 'http://www.cttdvnatl.net/gallery/img/core-img/ThaoLe.jpg',
+        name: 'Theresa Nguyễn Lệ Thảo',
+        email: 'lethao.theresa@cttdvnatl.org',
+        class: '2A',
         comment: [``]
     },
     {   
         classification: 'assistant-teacher',
-        image: 'Thư Ký Văn Phòng',
-        name: 'Teacher Name',
-        email: 'email@gmail.com',
-        class: '1A',
-        comment: [``]
-    },
-    {   
-        classification: 'assistant-teacher',
-        image: 'Thư Ký Văn Phòng',
-        name: 'Teacher Name',
-        email: 'email@gmail.com',
-        class: '1A',
+        image: 'http://www.cttdvnatl.net/gallery/img/core-img/VanPham.jpg',
+        name: 'Gioan Phạm Khoa Văn',
+        email: 'van.k.pham@cttdvnatl.org',
+        class: '2A',
         comment: [``]
     },
 ]
