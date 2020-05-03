611e9746ebbf9a0047b9a3124e335aacd2c34919
An Pham <anpham@cttdvnatl.org>
2020-05-03 16:52:42 -0400

    VietHong document page edit

--- src/components/DisplayDocuments.jsx
+++ src/components/DisplayDocuments.jsx
@@ -6,6 +6,7 @@ const DisplayDocuments = ({list}) => (
         <div class="col-10 col-md-6 col-lg-3">
         <div class="about-us-content mb-100">
             <a href={Documents.downloadLink} download><img height="auto" src={Documents.image} alt=""></img></a>
+            <h4>{Documents.description}</h4>
         </div>
         </div>
     ))}
