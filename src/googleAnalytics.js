af1310bdb382cc361481101e8495fd1966990ef7
An Pham <anpham@cttdvnatl.org>
2020-06-01 17:13:30 -0400

    google analytics

--- src/googleAnalytics.js
+++ src/googleAnalytics.js
@@ -3,11 +3,3 @@ window.dataLayer = window.dataLayer || [];
   gtag('js', new Date());
 
   gtag('config', 'UA-166941054-1');
-
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  ([r].q=i[r].q||[]).push(arguments)},i[r].I=1*new Date();a=s.createElement(o).
-  m=s.getElementsByTagNa,e(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create','UA-166941054-1', 'auto');
-ga('send','pageview');
