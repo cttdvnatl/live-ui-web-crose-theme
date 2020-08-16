db19d2c7ea95148dccaffd28505ca22011b13ed1
An Pham <anpham@cttdvnatl.org>
2020-08-16 17:04:12 -0400

    working on classes list for viet hong school

--- src/components/DisplayClasses.jsx
+++ src/components/DisplayClasses.jsx
@@ -6,9 +6,9 @@ const Classes = ({list}) => (
     <div class="row about-content justify-content-center">
     {list.map((Classes) => (
         <div class="col-10 col-md-6 col-lg-2">
-            <div style={{width: "250px"}} class="about-us-content mb-50">
-                <h3 style={{fontSize: "19px"}}>{Classes}Lớp </h3>
-                <h3 style={{fontSize: "19px"}}></h3>
+            <div style={{width: "250px"}} class="about-us-content mb-50" id={Classes.grade}>
+                <h3 style={{fontSize: "25px"}}>Lớp {Classes.class}</h3>
+                <h6>{Classes.teachers}</h6>
             </div>
         </div>
     ))}
---
+++ src/database/vietHongClasses.js
@@ -0,0 +1,65 @@
+const Classes = [
+    {
+        grade: "grade1",
+        class: "1A",
+        teachers: "Teacher1, Teacher2"
+    },
+    {
+        grade: "grade1",
+        class: "1B",
+        teachers: "Teacher1, Teacher2"
+    },
+    {
+        grade: "grade1",
+        class: "1C",
+        teachers: "Teacher1, Teacher2"
+    },
+
+    {
+        grade: "grade1",
+        class: "1D",
+        teachers: "Teacher1, Teacher2"
+    },    {
+        grade: "grade2",
+        class: "2A",
+        teachers: "Teacher1, Teacher2"
+    },
+    {
+        grade: "grade2",
+        class: "2B",
+        teachers: "Teacher1, Teacher2"
+    },
+    {
+        grade: "grade2",
+        class: "2C",
+        teachers: "Teacher1, Teacher2"
+    },
+    {
+        grade: "grade2",
+        class: "2D",
+        teachers: "Teacher1, Teacher2"
+    },
+    {
+        grade: "grade3",
+        class: "3A",
+        teachers: "Teacher1, Teacher2"
+    },
+    {
+        grade: "grade3",
+        class: "3B",
+        teachers: "Teacher1, Teacher2"
+    },
+    {
+        grade: "grade3",
+        class: "3C",
+        teachers: "Teacher1, Teacher2"
+    },
+    {
+        grade: "grade3",
+        class: "3D",
+        teachers: "Teacher1, Teacher2"
+    },
+
+]
+
+export default Classes;
