3c8c1d01057a9d60b32e58ad2d44f82264d6a40e
An Pham <anpham@cttdvnatl.org>
2020-08-19 19:08:08 -0400

    viethong classwork page

--- src/pages/VietHongClassworkPage.jsx
+++ src/pages/VietHongClassworkPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import vietHongClasses from '../database/vietHongClasses.js';
+import VietHongHeader from '../components/VietHongHeader';
 
 const VietHongClassworkPage = ({match}) => {
     const vietHongClassURL = match.params.vietHongClassURL;
@@ -10,8 +11,9 @@ const VietHongClassworkPage = ({match}) => {
     return (
         <>
         <Header/>
-        <div class="classwork">
-            <iframe width="80%" height="700px" style={{marginTop: "10px", border: "0"}} title="vh" src={iframe.classwork}/>
+        <VietHongHeader/>
+        <div class="center-50">
+            <iframe width="80%" height="700px" style={{margin: "0", padding: "0", border: "0"}} title="vh" src={iframe.classwork}/>
         </div>
         <Footer/>
         </>
