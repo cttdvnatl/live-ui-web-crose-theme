4da9e8fea3ea47b99096860b87a739dd6b59a497
gwynevere <tnghiem@cttdvnatl.org>
2020-08-21 00:08:44 -0400

    updating Viet Hong classes and documents page

--- src/components/DisplayClasses.jsx
+++ src/components/DisplayClasses.jsx
@@ -5,8 +5,8 @@ const Classes = ({list}) => (
     <div class="container">
     <div class="row about-content justify-content-center">
     {list.map((Classes) => (
-        <div class="school-classes col-10 col-md-6 col-lg-2">
-            <div style={{width: "250px"}} class="about-us-content mb-50">
+        <div class="school-classes col-10 col-md-6 col-lg-4">
+            <div style={{width: "350px"}} class="about-us-content mb-50">
                 <a id="classwork" href={"/viet-hong-classwork/" + Classes.name} style={{fontSize: "25px", fontWeight: "700"}}>Lớp {Classes.class}</a>
                 <h6>{Classes.teachers}
                     <p class="vh-hover-text">{Classes.emails}</p>                
