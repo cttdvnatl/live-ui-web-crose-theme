ceb5192dfe93550d188e08a845c309397d2f345d
An Pham <anpham@cttdvnatl.org>
2020-09-04 00:24:25 -0400

    translated viet hong header and homepage

--- src/components/VietHongHeader.jsx
+++ src/components/VietHongHeader.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { useTranslation } from 'react-multi-lang';
 
-const VietHongHeader = () => (
-    <>
-    <div id="viet-hong-header">
-        <a href="/viethong">Trường Việt Hồng</a>
-        <a href="/viet-hong-teachers">Thầy Cô</a>
-        <a href="/viet-hong-activities">Sinh Hoạt</a>
-        <a href="/viet-hong-documents">Văn Thư</a>
-        <a href="/viet-hong-classes">Các Lớp</a>
-    </div>
-    </>
-);
+const VietHongHeader = () => {
+    const t = useTranslation();
+    return (
+        <div id="viet-hong-header">
+            <a href="/viethong">{t('viethong.header.item1')}</a>
+            <a href="/viet-hong-teachers">{t('viethong.header.item2')}</a>
+            <a href="/viet-hong-activities">{t('viethong.header.item3')}</a>
+            <a href="/viet-hong-documents">{t('viethong.header.item4')}</a>
+            <a href="/viet-hong-classes">{t('viethong.header.item5')}</a>
+        </div>
+    )
+
+}
 
 export default VietHongHeader;
