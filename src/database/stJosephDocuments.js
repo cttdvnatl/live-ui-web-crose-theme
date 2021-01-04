44de5ad8c25ffcd29f0e3ee29423a14ec5b84bca
gwynevere <tnghiem@cttdvnatl.org>
2021-01-04 18:10:39 -0500

    some changes to the documents sections of Giáo Lý and Việt Hồng

--- src/database/stJosephDocuments.js
+++ src/database/stJosephDocuments.js
@@ -1,8 +1,13 @@
 const stJosephDocuments = [
     {   
+        fileName: 'Sunday School Schedule',
+        downloadFile: 'https://hvmatl.net/gallery/kgd/giaoly/docs-St.Joseph-School-2020-2021-Schedule.pdf',
+        description: 'Chương Trình Sinh Hoạt Giáo Lý 2020-2021',
+    },
+    {   
         fileName: 'Sunday School Application',
-        downloadFile: 'https://hvmatl.net/gallery/kgd/giaoly/docs-Don_Ghi_Danh_Blank_2020-2021.doc',
-        description: 'Đơn Ghi Danh Học Giáo Lý Niên Học 2020-2021',
+        downloadFile: 'https://hvmatl.net/gallery/kgd/giaoly/docs-St.Joseph-School-2020-2021-Don_Ghi_Danh_Blank.doc',
+        description: 'Đơn Ghi Danh Học Giáo Lý 2020-2021',
     },
     {   
         fileName: 'Permission to Contact Youth',
