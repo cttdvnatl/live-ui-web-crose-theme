e5ec3ef4af4d41c272b5a39f352ca774992cea08
vkpham <56766653+vkpham@users.noreply.github.com>
2021-02-07 09:19:44 -0500

    Update ActivitiesPage.jsx

--- src/pages/ActivitiesPage.jsx
+++ src/pages/ActivitiesPage.jsx
@@ -7,7 +7,7 @@ const ActivitiesPage = () => (
     <Header />
     <section className="section-padding-25">
         <h2 className="section-heading">Sinh Hoạt Giáo Xứ</h2>
-        <iframe className="center-50" title="Sinh Hoạt Giáo Xứ" src="https://docs.google.com/document/d/e/2PACX-1vTCkv4mTVvg9yKPcs3CjZS7B10N2nOauT7p6ESwvGQGBYHiP-QsZzvI1ZRN7vrz0g/pub?embedded=true"
+        <iframe className="center-50" title="Sinh Hoạt Giáo Xứ" src="https://docs.google.com/document/d/16bWSHAs9ToINtqDxRll40msC2P7ptzqQ92JpXCXurTY/edit?usp=sharing"
             frameBorder="0"
             style={{'width':"90%", 'height':"1000px"}}/>
     </section>
