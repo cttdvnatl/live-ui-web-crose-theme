8ebf0f303ec94f1a12ca4e2c3a38b049298f16e5
Jomar HA Nguyen <jomarhan.ng@gmail.com>
2021-04-09 21:14:09 -0400

    start styling write-article page

---
+++ src/pages/WriteArticle.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+
+function WriteArticle() {
+    return (
+        <div>
+            <Header/>
+            <p>Hello heere</p>
+            <Footer/>
+        </div>
+    )
+}
+
+export default WriteArticle
