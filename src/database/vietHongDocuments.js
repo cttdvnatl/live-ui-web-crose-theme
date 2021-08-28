5c4566333777792cc6f481aa2f2a28e9c0965a4f
gwynevere <tnghiem@cttdvnatl.org>
2021-08-28 01:09:29 -0400

    updates for Việt Hồng - 28-08-2021

--- src/database/vietHongDocuments.js
+++ src/database/vietHongDocuments.js
@@ -1,6 +1,16 @@
 const vietHongDocuments = [
 
     {   
+        fileName: 'School Year Calendar',
+        downloadFile: 'https://hvmatl.net/gallery/kgd/viethong/Viet-Hong-School-2021-2022-School-Year-Calendar.pdf',
+        description: 'Lịch Trình Niên Học 2021-2022',
+    },
+    {   
+        fileName: 'Student Absence Form',
+        downloadFile: 'https://hvmatl.net/gallery/kgd/viethong/Viet-Hong-School-2021-2022-Absence-Form.pdf',
+        description: 'Giấy Xin Phép Vắng Mặt',
+    },
+    {   
         fileName: 'Permission to Contact Youth',
         downloadFile: 'https://docs.google.com/forms/d/e/1FAIpQLScNXxHFs6fdvLQWp2tJ45VJP37MOWO8Sy2czea6F1-7TtZEFg/viewform',
         description: 'Giấy Phép Xin Liên Lạc Với Trẻ Vị Thành Niên',
