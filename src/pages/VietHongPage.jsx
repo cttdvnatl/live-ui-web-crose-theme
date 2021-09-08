a081467e422782632518881b50f51101d04d0f90
gwynevere <tnghiem@cttdvnatl.org>
2021-09-07 21:30:24 -0400

    updates for Việt Hồng - 09-07-2021

--- src/pages/VietHongPage.jsx
+++ src/pages/VietHongPage.jsx
@@ -12,18 +12,16 @@ const VietHong = () => {
             <Header/>
             <VietHongHeader/>
             <br/><br/><br/>
-            <h5 style={{textAlign: "center"}}> Nhà trường chúng tôi mong nhận được nhiều giúp đỡ, thông cảm và cộng tác <br/>
-                từ quý phụ huynh để chúng ta cùng có được một năm học thành công  <br/>
-                cho con em mình. Xin cảm ơn quý vị. <br/><br/>
-
-                Theo quyết định của Tổng Giáo Phận Atlanta và Học Khu Gwinnett <br/>
-                các em học sinh cần phải mang face mask cho đến khi có thông báo mới. <br/><br/>
-
-                Tin tức và thay đổi trong niên học <br/>
-                sẽ được cập nhật thường xuyên trong trang này. <br/><br/>
-
-                </h5>
-                
+            <h5 style={{ textAlign: "center" }}>{t('viethong.home.heading')}<br/>
+                {t('viethong.home.line1')} <br/>
+                {t('viethong.home.line2')} <br/><br/>
+                {t('viethong.home.line3')} <br/>                 
+                {t('viethong.home.line4')} <br/>
+                {t('viethong.home.line5')} <br/><br/>
+                {t('viethong.home.line6')} <br/>
+                {t('viethong.home.line7')} 
+                <br/><br/>
+                </h5>              
                 <div className="container-fluid text-center">
                 <p id="viet-ngu" style={{width: '65%', margin: 'auto'}}>{t('viethong.home.announcement')}</p>
                 <br/>
