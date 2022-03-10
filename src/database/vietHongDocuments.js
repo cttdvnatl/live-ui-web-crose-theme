1ceb8288f436ecfc599843480b4dbc09ac33d8e7
gwynevere <tnghiem@cttdvnatl.org>
2022-03-10 15:00:06 -0500

    updates for Việt Hồng - 03-10-2022

--- src/database/vietHongDocuments.js
+++ src/database/vietHongDocuments.js
@@ -1,6 +1,16 @@
 const vietHongDocuments = [
 
     {   
+        fileName: 'RETURNING Student Registration Form',
+        downloadFile: 'https://hvmatl.net/gallery/kgd/viethong/Viet-Hong-School-2022-23-RETURNING-Student-and-School-Policies.pdf',
+        description: 'Đơn Ghi Danh Học Sinh Cũ và Nội Quy 2022-2023',
+    },
+    {   
+        fileName: 'NEW Student Registration Form',
+        downloadFile: 'https://hvmatl.net/gallery/kgd/viethong/Viet-Hong-School-2022-23-NEW-Student-and-School-Policies.pdf',
+        description: 'Đơn Ghi Danh Học Sinh Mới và Nội Quy 2022-2023',
+    },
+    {   
         fileName: 'Parents Handbook',
         downloadFile: 'https://hvmatl.net/gallery/kgd/viethong/Viet-Hong-School-2021-2022-Parents-Handbook-bilingual.pdf',
         description: 'Sổ Tay Phụ Huynh 2021-2022',
