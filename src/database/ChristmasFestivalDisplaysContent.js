072aa62f2023d813604308bf69d29fb057523d52
only-gwynevere <65273602+only-gwynevere@users.noreply.github.com>
2022-11-30 08:38:40 -0500

    added a new photo to Christmas Festival 2022 page - 11-30-2022

--- src/database/ChristmasFestivalDisplaysContent.js
+++ src/database/ChristmasFestivalDisplaysContent.js
@@ -1,7 +1,7 @@
 const ChristmasFestivalDisplaysContent = [
     {
         classification: 'main-display',
-        img: "https://www.hvmatl.com/gallery/2022_LHGS/2022_LHGS_Logo.jpg",
+        img: "https://www.hvmatl.com/gallery/2022_LHGS/2022_LHGS_Church.png",
         caption: ""
     },
 
