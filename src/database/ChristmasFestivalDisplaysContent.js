9a948861affc6acbb3fa64c2d77db08c245211b1
only-gwynevere <65273602+only-gwynevere@users.noreply.github.com>
2022-11-30 09:32:53 -0500

    added posters to Christmas Festival - 11-30-2022

--- src/database/ChristmasFestivalDisplaysContent.js
+++ src/database/ChristmasFestivalDisplaysContent.js
@@ -1,7 +1,12 @@
 const ChristmasFestivalDisplaysContent = [
     {
         classification: 'main-display',
-        img: "https://www.hvmatl.com/gallery/2022_LHGS/2022_LHGS_Church.png",
+        img: "https://www.hvmatl.com/gallery/2022_LHGS/2022_LHGS_Nativity_Displays_Rules.jpg",
+        caption: ""
+    },
+    {
+        classification: 'main-display',
+        img: "https://www.hvmatl.com/gallery/2022_LHGS/2022_LHGS_Nativity_Displays_Location.jpg",
         caption: ""
     },
 
