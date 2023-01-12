7c90b2cf5e03e7a81e8e6e3fdeb9113f64486e4e
josephmai19 <joeymai@hvmatl.org>
2023-01-11 21:51:59 -0500

    Update mass times and add translation

--- src/pages/MassSchedule.jsx
+++ src/pages/MassSchedule.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import Preloader from "../components/Preloader";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { useTranslation } from 'react-multi-lang';
 
 const MassSchedule = (prop) => {
+    const t = useTranslation()
     return (
         <div>
             <Preloader/>
@@ -11,34 +13,19 @@ const MassSchedule = (prop) => {
             <section className="section-padding-100">
                 <div className="container">
                     <div className="row">
-                        <div className="col-sm-6 text-center">
+                         <div className="col-sm-12 text-center">
                             <p>
-                                <h3>Giờ Lễ</h3>
-                                <b>Thứ Hai - Thứ Bảy:</b>  8:00AM<br/>
-                                <b>Thứ Năm - Thứ Sáu:</b>  7:00PM<br/>
-                                <b>Thứ Bảy:</b>  Lễ Vọng Chúa Nhật 5:30PM<br/>
-                                <b>Chúa Nhật:</b>  7:30AM , 9:30AM , & 11:30AM<br/><br/>
-                                <h3>Giải Tội</h3>
-                                <b>Ngày Thường:</b> Sau Thánh Lễ  8:00AM<br/>
-                                <b>Thứ Bảy:</b>  4:30PM - 5:15PM<br/><br/><br/>
-                                <h3>Rửa Tội Trẻ Em</h3>
-                                <b>Chúa Nhật Tuần Thứ 1:</b>  9:30AM - 10:30AM<br/>
-                                <b>Thứ Bảy Tuần Thứ 2:</b> Sau Lễ 8:00AM<br/><br/><br/>
-                            </p>
-                        </div>
-                        <div className="col-sm-6 text-center">
-                            <p>
-                                <h3>Mass Times</h3>
-                                <b>Monday - Saturday:</b>  8:00AM<br/>
-                                <b>Thurday - Friday:</b>  7:00PM<br/>
-                                <b>Saturday:</b>  Vigil Mass 5:30PM<br/>
-                                <b>Sunday:</b>  7:30AM , 9:30AM , & 11:30AM<br/><br/>
-                                <h3>Reconciliation (Confession)</h3>
-                                <b>Weekdays:</b> following 8:00AM Mass<br/>
-                                <b>Saturday:</b>  4:30PM - 5:15PM<br/><br/><br/>
-                                <h3>Baptisms</h3>
-                                <b>1st Sunday of Month (Mandatory Class):</b>  9:30AM - 10:30AM<br/>
-                                <b>2nd Saturday of Month (Baptism):</b> After Mass 8:00AM<br/><br/><br/>
+                                <h3>{t('massInfo.mass.title')}</h3>
+                                <b>{t('massInfo.mass.weekdayMorning')}</b><br/>
+                                <b>{t('massInfo.mass.weekdayNight')}</b><br/>
+                                <b>{t('massInfo.mass.vigil')}</b><br/>
+                                <b>{t('massInfo.mass.sunday')}</b><br/><br/>
+                                <h3>{t('massInfo.confession.title')}</h3>
+                                <b>{t('massInfo.confession.weekday')}</b><br/>
+                                <b>{t('massInfo.confession.weekend')}</b><br/><br/><br/>
+                                <h3>{t('massInfo.baptism.title')}</h3>
+                                <b>{t('massInfo.baptism.class')}</b><br/>
+                                <b>{t('massInfo.baptism.baptise')}</b><br/><br/><br/>
                             </p>
                         </div>
                     </div>
