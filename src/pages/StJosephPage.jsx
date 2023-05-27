59a84aff1af075bf0d45de7a7041495c8ba9ae8f
only-gwynevere <65273602+only-gwynevere@users.noreply.github.com>
2023-05-26 20:58:48 -0400

    2023-05-26 - updates for Giao Ly + Viet Ngu

--- src/pages/StJosephPage.jsx
+++ src/pages/StJosephPage.jsx
@@ -15,26 +15,17 @@ const StJoseph = () => {
             <h5 style={{ textAlign: "center" }}>{t('stjoseph.home.heading')}<br/><br/>
                 {t('stjoseph.home.line1')} <br/>
                 {t('stjoseph.home.line2')} <br/>
-                {t('stjoseph.home.line3')} <br/><br/>               
+                {t('stjoseph.home.line3')} <br/>              
                 {t('stjoseph.home.line4')} <br/><br/>
-                {t('stjoseph.home.line5')} <br/><br/>
+                {t('stjoseph.home.line5')} <br/>
                 {t('stjoseph.home.line6')} <br/>
                 {t('stjoseph.home.line7')} <br/><br/>
                 {t('stjoseph.home.line8')} <br/>
                 {t('stjoseph.home.line9')} <br/>
-                {t('stjoseph.home.line10')} <br/>
+                {t('stjoseph.home.line10')} <br/><br/> 
                 {t('stjoseph.home.line11')} <br/>
                 {t('stjoseph.home.line12')} <br/>
-                {t('stjoseph.home.line13')} <br/>
-                {t('stjoseph.home.line14')} <br/><br/>
-                {t('stjoseph.home.line15')} <br/><br/>
-                {t('stjoseph.home.line16')} <br/>
-                {t('stjoseph.home.line17')} <br/><br/>
-                {t('stjoseph.home.line18')} <br/><br/>
-                {t('stjoseph.home.line19')} <br/>
-                {t('stjoseph.home.line20')} <br/><br/>
-                {t('stjoseph.home.line21')} <br/><br/>
-                {t('stjoseph.home.line22')} <br/><br/>
+                {t('stjoseph.home.line13')} <br/><br/> 
                 </h5>
                 <div className="container-fluid text-center">
                     <p id="viet-ngu" style={{width: '65%', margin: 'auto'}}>{t('stjoseph.home.announcement')}</p>
