c0810b5030f81e73a79a5d4bd6c781d35a5cd328
only-gwynevere <65273602+only-gwynevere@users.noreply.github.com>
2023-08-10 00:30:06 -0400

    2023-08-10 - updates for Viet Hong

--- src/database/vietHongClasses.js
+++ src/database/vietHongClasses.js
@@ -1,101 +1,108 @@
 const Classes = [
     {
-        name: "mg",
-        class: "Mẫu Giáo",
-        teachers: "Cô Dương Hiền, Cô Võ Mercy Sarah, Thầy Nguyễn Thanh Sơn",
+        name: "mga",
+        class: "Mẫu Giáo A",
+        teachers: "Cô Dương Hiền, Cô Nguyễn T. Vy Christine, Thầy Nguyễn Thanh Sơn",
         emails: "",
-        classwork: "https://docs.google.com/document/d/e/2PACX-1vSmbzolKGSy-E7YzX68TPr1xzZVH2XzI1DlWvkXYsjQbZO3a2wD-p-kO16Mm_Y3e6vfuwLgDW4vwBnk/pub?embedded=true"
+        classwork: ""
+    },
+    {
+        name: "mgb",
+        class: "Mẫu Giáo B",
+        teachers: "Cô Võ Mercy Sarah, Cô Lê Nga, Cô Vũ Kim Liên",
+        emails: "",
+        classwork: ""
     },
     {
         name: "vla",
         class: "Vỡ Lòng A",
-        teachers: "Cô Lu Aileen, Cô Nguyễn Thị Ái Mỹ, Cô Trần Trang",
+        teachers: "Cô Đặng Thị Ngọc Dung, Cô Huỳnh Bảo Trân",
         emails: "",
-        classwork: "https://docs.google.com/document/d/e/2PACX-1vT4_8J1SooTMegrDMFNFHHDAs-Lp-6GI_25QjJgxArQhQz9GT8WGAUJPt1O7fuVFoc3hhaU2dYlaug4/pub?embedded=true"
+        classwork: ""
     },
     {
         name: "vlb",
         class: "Vỡ Lòng B",
-        teachers: "Cô Lê Bích Duyên, Cô Trần Trúc Quỳnh",
+        teachers: "Cô Lê Bích Duyên, Cô Nguyễn Thị Vân",
         emails: "",
-        classwork: "https://docs.google.com/document/d/e/2PACX-1vRqCicokmDNzw1uBgBlr3pjHt-093law2fs8biDPe7bh6dNp2DNmaw0gqIxUdGAu8lLNhzHGxqxzjq_/pub?embedded=true"
+        classwork: ""
     },
     {
         name: "vlc",
         class: "Vỡ Lòng C",
-        teachers: "Cô Đặng Thị Ngọc Dung, Cô Huỳnh Bảo Trân, Cô Vũ Kim Liên",
+        teachers: "Cô Phạm T. Nga, Thầy Nguyễn Joseph Alex",
         emails: "",
-        classwork: "https://docs.google.com/document/d/e/2PACX-1vRBC1Bdtl0aqxlHQOhZrgaC1YLqSrwsYDTobLhwz0CzMSkY7g447acIPfA5Z7G9TLC9NgybkGQStt0b/pub?embedded=true"
+        classwork: ""
     },
     {
         name: "vld",
         class: "Vỡ Lòng D",
-        teachers: "Cô Phạm T. Nga, Cô Nguyễn Thị Vân, Cô Lê Ngọc Diễm, Cô Mai Trí Linh",
+        teachers: "Cô Trần Trúc Quỳnh, Cô Đặng Kim, Cô Huỳnh Thi Trang",
         emails: "",
-        classwork: "https://docs.google.com/document/d/e/2PACX-1vSVMDgIfwdDu1sHGHDOvNyW8PJ427uTphU4A9Bfz0f9UDEiG6SY3h8pe-nTeTEN8AGDUiAQEfFNTlva/pub?embedded=true"
+        classwork: ""
     },
     {
         name: "1a",
         class: "1A",
         teachers: "Cô Nguyễn Minh Viên",
         emails: "",
-        classwork: "https://docs.google.com/document/d/e/2PACX-1vR0_TX2LZen-z3ml7303ShaenfhKI7PmBGdFcpv4NmG_fPLBggf9RJ2NiOw6R1f6hJhkGMbSUBT3pGf/pub?embedded=true"
+        classwork: ""
     },
     {
         name: "1b",
         class: "1B",
-        teachers: "Cô Phan Tùng, Thầy Lê Đại Trí",
+        teachers: "Cô Phan Tùng",
         emails: "",
-        classwork: "https://docs.google.com/document/d/e/2PACX-1vR4M9rnlSDWt0UH0n_pdXRTuhfA0_33BJ52r5CIz3aoeV_B6qSvae1zqtjNh54YeM_aTDbaBUxR3wRN/pub?embedded=true"
+        classwork: ""
+    },
+    {
+        name: "1c",
+        class: "1C",
+        teachers: "Cô Nguyễn Thùy Linh, Cô Nguyễn Ngọc Châu",
+        emails: "",
+        classwork: ""
     },
     {
         name: "2",
         class: "2",
-        teachers: "Cô Nguyễn Huỳnh Anh, Cô Nguyễn Thùy Linh", 
+        teachers: "Cô Trần Uyên Chi, Cô Võ Tuyết Trân", 
         emails: "",
-        classwork: "https://docs.google.com/document/d/e/2PACX-1vSe8he7RZPMhSmV_YoFSB8EQAv3WyS9GQM1N5viaxc0WAkHLGz1uQ7nAUhwg-y3_iZVpFcW4DNnaanl/pub?embedded=true"
+        classwork: ""
     },
     {
         name: "3",
         class: "3",
-        teachers: "Cô Trần Uyên Chi, Thầy Nguyễn Quốc Tuấn",
+        teachers: "Thầy Nguyễn Quốc Tuấn, Thầy Nguyễn Hữu Toàn",
         emails: "",
-        classwork: "https://docs.google.com/document/d/e/2PACX-1vRAiwf8LEdCeuWVDWkLXiwH3sWJs-2ctl84qdZ5zaZF02f3MeoIQu9ZW8Xrn7zidVfwsyPB7D1zVUUk/pub?embedded=true"
+        classwork: ""
     },
     {
         name: "4",
         class: "4",
-        teachers: "Thầy Võ Minh Hoàng, Thầy Nguyễn Hữu Toàn, Cô Trần Lan Bình",
+        teachers: "Thầy Võ Minh Hoàng, Cô Trần Lan Bình",
         emails: "",
-        classwork: "https://docs.google.com/document/d/e/2PACX-1vSSZ6eiKjVc5R7H63hZAP7g66mthjiab1xq8vfPI3oll-CkuvkE_jW-B41Um_h_YohiMiZ16KSgyyzZ/pub?embedded=true"
+        classwork: ""
     },
     {
         name: "5",
         class: "5",
         teachers: "Thầy Trần Jimmy Việt, Thầy Đào Hoàng Phi",
         emails: "",
-        classwork: "https://docs.google.com/document/d/e/2PACX-1vSprkeKB76oZOj_CSYFYlmNJq9gz23VxBfWX7zbVCgyIVnfKdBuS2AZ_IsiZpHSIpYz4LclR1bmzKUD/pub?embedded=true"
+        classwork: ""
     },
     {
         name: "6",
         class: "6",
-        teachers: "Thầy Phạm Hồng Trung, Thầy Lê Steve",
+        teachers: "Thầy Phạm Hồng Trung, Thầy Lê Steve. Thầy Hoàng Tuấn Long",
         emails: "",
-        classwork: "https://docs.google.com/document/d/e/2PACX-1vSEIsprmho1ExE8o6gIWu33vW6Xdurj4nA_RY4DqdAR2hWfH62G6IPaQO-0rlOtSIUIwVLupTiWA5jl/pub?embedded=true"
+        classwork: ""
     },
     {
         name: "7",
         class: "7",
-        teachers: "Thầy Ninh Q. Đạt, Thầy Lê Tân",
+        teachers: "Thầy Ninh Q. Đạt",
         emails: "",
-        classwork: "https://docs.google.com/document/d/e/2PACX-1vQY-_apFhjHxRbg2-IxWBDavJ-Vq5zpXRh5N1vw1PCbxOcn0QgF6-o3mv83wG4ND99JB6fKYAzkRV6n/pub?embedded=true"
-    },
-    {
-        name: "pg",
-        class: "Phụ Giáo",
-        teachers: "Thầy Trần Thanh Trúc, Cô Nghiêm Trang",
-        emails: "",
-        classwork: "https://docs.google.com/document/d/e/2PACX-1vRt-ZoiqXI7ng2x57z_DZajtZU-2vm2NGU8xyhrmBkFIBhsqXLdkfwFaOI6q8hsT0EpiKHymEy7aqKq/pub?embedded=true"
+        classwork: ""
     },
 ]
 
