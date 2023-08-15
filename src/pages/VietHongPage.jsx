b20450b91abd94feca7f31323f1faf2180130aee
only-gwynevere <65273602+only-gwynevere@users.noreply.github.com>
2023-08-14 22:05:02 -0400

    2023-08-14 - updates for Giao Ly + Viet Hong

--- src/pages/VietHongPage.jsx
+++ src/pages/VietHongPage.jsx
@@ -8,21 +8,23 @@ import VietHongCalendar from '../components/VietHongCalendar';
 const VietHong = () => {
     const t = useTranslation();
     return (
-        <div>
-            <Header/>
+        <div>   
+        <Header/>
             <VietHongHeader/>
             <br/><br/><br/>
             <h5 style={{ textAlign: "center" }}>{t('viethong.home.heading')}<br/><br/> 
                 {t('viethong.home.line1')} <br/>
-                {t('viethong.home.line2')} <br/>
-                {t('viethong.home.line3')} <br/><br/>     
+                {t('viethong.home.line2')} <br/> 
+                {t('viethong.home.line3')} <br/><br/>        
+                {t('viethong.home.line4')} <br/><br/> 
+                {t('viethong.home.line5')} <br/><br/><br/>
                 </h5>              
                 <div className="container-fluid text-center">
                 <p id="viet-ngu" style={{width: '65%', margin: 'auto'}}>{t('viethong.home.announcement')}</p>
                 <br/>
                 <div className="container-fluid text-center">
 
-                <p id="vn-schedule" className="center-50"> <b>Giờ Học / School Hours</b> <br/>
+                <p id="vn-schedule" className="center-50"> <b>Giờ học mỗi thứ Bảy</b> <br/>
                 10:00 a.m. – 12:15 p.m. </p>
                 </div>
                 <VietHongCalendar />
