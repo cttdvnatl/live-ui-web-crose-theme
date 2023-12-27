84e57db055cae797621ad0f1cdb537e4a0f871bf
only-gwynevere <65273602+only-gwynevere@users.noreply.github.com>
2023-12-26 22:02:17 -0500

    2023-12-26 - updates for Christmas Festival

--- src/database/ChristmasFestivalDisplaysContent.js
+++ src/database/ChristmasFestivalDisplaysContent.js
@@ -4,51 +4,6 @@ const ChristmasFestivalDisplaysContent = [
         img: "https://hvmatl.s3.us-east-2.amazonaws.com/gallery/LHGS/2023-Christmas-festival-navitity-displays-poster.jpg",
         caption: ""
     },
-    {
-        classification: 'main-display',
-        img: "https://www.hvmatl.com/gallery/2022_LHGS/2022_LHGS_Nativity_Displays-02.jpg",
-        caption: ""
-    },
-    {
-        classification: 'main-display',
-        img: "https://www.hvmatl.com/gallery/2022_LHGS/2022_LHGS_Nativity_Displays-03.jpg",
-        caption: ""
-    },
-    {
-        classification: 'main-display',
-        img: "https://www.hvmatl.com/gallery/2022_LHGS/2022_LHGS_Nativity_Displays-04.jpg",
-        caption: ""
-    },
-    {
-        classification: 'main-display',
-        img: "https://www.hvmatl.com/gallery/2022_LHGS/2022_LHGS_Nativity_Displays-05.jpg",
-        caption: ""
-    },
-    {
-        classification: 'main-display',
-        img: "https://www.hvmatl.com/gallery/2022_LHGS/2022_LHGS_Nativity_Displays-06.jpg",
-        caption: ""
-    },
-    {
-        classification: 'main-display',
-        img: "https://www.hvmatl.com/gallery/2022_LHGS/2022_LHGS_Nativity_Displays-07.jpg",
-        caption: ""
-    },
-    {
-        classification: 'main-display',
-        img: "https://www.hvmatl.com/gallery/2022_LHGS/2022_LHGS_Nativity_Displays-08.jpg",
-        caption: ""
-    },
-    {
-        classification: 'main-display',
-        img: "https://www.hvmatl.com/gallery/2022_LHGS/2022_LHGS_Nativity_Displays_Rules.jpg",
-        caption: ""
-    },
-    {
-        classification: 'main-display',
-        img: "https://www.hvmatl.com/gallery/2022_LHGS/2022_LHGS_Nativity_Displays_Location.jpg",
-        caption: ""
-    },
 
 ]
 
--- src/database/ChristmasFestivalMusicContent.js
+++ src/database/ChristmasFestivalMusicContent.js
@@ -6,30 +6,10 @@ const ChristmasFestivalMusicContent = [
     },
     {
         classification: 'main-music',
-        img: "https://www.hvmatl.com/gallery/2022_LHGS/2022_LHGS_Music-02.jpg",
+        img: "https://hvmatl.s3.us-east-2.amazonaws.com/gallery/LHGS/2023-Christmas-festival-handdrawn.jpg",
         caption: ""
     },
-    {
-        classification: 'main-music',
-        img: "https://www.hvmatl.com/gallery/2022_LHGS/2022_LHGS_Music-03.jpg",
-        caption: ""
-    },
-    {
-        classification: 'main-music',
-        img: "https://www.hvmatl.com/gallery/2022_LHGS/2022_LHGS_Music-04.jpg",
-        caption: ""
-    },
-    {
-        classification: 'main-music',
-        img: "https://www.hvmatl.com/gallery/2022_LHGS/2022_LHGS_Music-05.jpg",
-        caption: ""
-    },
-    {
-        classification: 'main-music',
-        img: "https://www.hvmatl.com/gallery/2022_LHGS/2022_LHGS_Music-06.jpg",
-        caption: ""
-    },
-
+    
 ]
 
 export default ChristmasFestivalMusicContent;
