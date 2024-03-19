5a46a329fabc6aa0f69216179b7807a91ffe616c
only-gwynevere <65273602+only-gwynevere@users.noreply.github.com>
2024-03-19 00:31:58 -0400

    2024-03-19 - updates for Education Department

--- src/pages/VietHongPage.jsx
+++ src/pages/VietHongPage.jsx
@@ -12,26 +12,20 @@ const VietHong = () => {
         <Header/>
             <VietHongHeader/>
             <br/><br/><br/>
-            <h5 style={{ textAlign: "center" }}>{t('stjoseph.home.heading')}<br/>
-                {t('stjoseph.home.line1')} <br/><br/>
-                {t('stjoseph.home.line2')} <br/>
-                {t('stjoseph.home.line3')} <br/>                 
-                {t('stjoseph.home.line4')} <br/><br/>
-                {t('stjoseph.home.line5')} <br/><br/>
-                {t('stjoseph.home.line6')} <br/>
-                {t('stjoseph.home.line7')} <br/>
-                {t('stjoseph.home.line8')} <br/>
-                {t('stjoseph.home.line9')} <br/><br/>
-                {t('stjoseph.home.line10')} <br/>
-                {t('stjoseph.home.line11')} <br/><br/>
-                {t('stjoseph.home.line12')} <br/>
-                {t('stjoseph.home.line13')} <br/>
-                {t('stjoseph.home.line14')} <br/>
-                {t('stjoseph.home.line15')} <br/><br/>
-                {t('stjoseph.home.line16')} <br/>
-                {t('stjoseph.home.line17')} <br/>
-                {t('stjoseph.home.line18')} <br/><br/><br/>
-                </h5>            
+            <h5 style={{ textAlign: "center" }}>{t('viethong.home.heading')}<br/><br/> 
+                {t('viethong.home.line1')} <br/>
+                {t('viethong.home.line2')} <br/> 
+                {t('viethong.home.line3')} <br/><br/>        
+                {t('viethong.home.line4')} <br/>
+                {t('viethong.home.line5')} <br/><br/>
+                {t('viethong.home.line6')} <br/>
+                {t('viethong.home.line7')} <br/><br/>
+                {t('viethong.home.line8')} <br/><br/>
+                {t('viethong.home.line9')} <br/>
+                {t('viethong.home.line10')} <br/>
+                {t('viethong.home.line11')} <br/>
+                {t('viethong.home.line12')} <br/><br/>
+                </h5>             
                 <div className="container-fluid text-center">
                 <p id="viet-ngu" style={{width: '65%', margin: 'auto'}}>{t('viethong.home.announcement')}</p>
                 <br/>
