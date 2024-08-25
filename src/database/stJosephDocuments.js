c6c53f19241caefb8e2b1f3d42d6725b1c1c738f
only-gwynevere <65273602+only-gwynevere@users.noreply.github.com>
2024-08-24 22:33:19 -0400

    2024-08-24 - updates for St. Joseph

--- src/database/stJosephDocuments.js
+++ src/database/stJosephDocuments.js
@@ -1,8 +1,13 @@
 const stJosephDocuments = [ 
     {   
-        fileName: 'School Year Education Department Schedule',
+        fileName: 'Education Department Parents Handbook',
+        downloadFile: 'https://hvmatl.s3.us-east-2.amazonaws.com/gallery/kgd/giaoly/2024-25-Education-Department-Parents-Handbook.pdf',
+        description: 'Khối Giáo Dục Sổ Tay Phụ Huynh 2024-2025',
+    },
+    {   
+        fileName: 'Education Department Schedule',
         downloadFile: 'https://hvmatl.s3.us-east-2.amazonaws.com/gallery/kgd/giaoly/2024-25-Education-Department-Calendar.pdf',
-        description: 'Khối Giáo Dục Lịch Trình Niên Học 2024-2025',
+        description: 'Khối Giáo Dục Lịch Trình 2024-2025',
     },
     {   
         fileName: 'Permission to Contact Youth',
